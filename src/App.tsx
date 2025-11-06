33b343e026cc6b658ca62c38ed4480e47318b0d4
Gersosa-18 <142690137+Gersosa-18@users.noreply.github.com>
2025-11-05 22:44:19 -0300

    feat: persistir sesión al recargar la app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import MainLayout from "./pages/MainLayout";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsAuth(!!token);
+    setLoading(false);
+  }, []);
+
+  if (loading) return <div>Cargando ...</div>;
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<MainLayout />} />
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={isAuth ? <Navigate to="/dashboard" /> : <Login />}
+        />
+        <Route
+          path="/dashboard"
+          element={isAuth ? <MainLayout /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/"
+          element={<Navigate to={isAuth ? "/dashboard" : "/login"} />}
+        />
       </Routes>
     </BrowserRouter>
   );
